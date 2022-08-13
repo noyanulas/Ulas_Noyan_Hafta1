3d80cdb8034b8fc7770027816581be084ee0410e
Ulaş Noyan <99445511+noyanulas@users.noreply.github.com>
2022-08-13 21:15:10 +0300

    Add files via upload

---
+++ UlasNoyan_Homework1/Controllers/FaizHesapController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UlasNoyan_Homework1.Controllers
+{
+    
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FaizHesapController : ControllerBase
+    {
+
+
+
+
+        [HttpGet("Anlık Bileşik Faiz Hesaplama")]
+        public string Get(float AnaPara, double FaizOrani, double DonemMiktari)
+        {
+            double anlik_Sureli = 2.71;
+            
+            for (int i = 0; i < DonemMiktari*FaizOrani - 1; i++)
+            {
+                anlik_Sureli *= 2.71;
+            }
+            double FaizGetirisi = AnaPara * anlik_Sureli;
+            return "Toplam Bakiye: " + Convert.ToString(FaizGetirisi) + " Toplam Kazanılan Faiz Tutarı: " + Convert.ToString(FaizGetirisi-AnaPara);
+             
+        }
+    }
+}
+
